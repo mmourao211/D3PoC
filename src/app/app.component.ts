69aded5bcbba3076c269f5c6a0db2a5be6b46c16
mmourao211 <sodwanted@gmail.com>
2017-10-24 16:55:12 +0300

    jointjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import * as d3 from "d3";
+import * as joint from 'jointjs'
+import * as _ from 'underscore';
 
 
 
@@ -14,51 +15,123 @@ export class AppComponent {
    *
    */
   constructor() {
-    var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g").attr("transform", "translate(" + (width / 2 + 40) + "," + (height / 2 + 90) + ")");
 
-var stratify = d3.stratify()
-    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+    var graph = new joint.dia.Graph;
+    
+    var paper = new joint.dia.Paper({
+    
+        el: $('#paper'),
+        width: 10000,
+        height: 10000,
+        gridSize: 1,
+        model: graph,
+        interactive: false
+        // interactive: {
+        //   arrowheadMove: false,
+        //   vertexMove: false,
+        //   vertexAdd: false,
+        //   vertexRemove: false,
+        //   labelMove: false,
+        //   useLinkTools: false
+        // }
+    });
+    
+function buildGraphFromAdjacencyList(adjacencyList) {
+  
+      var elements = [];
+      var links = [];
+      
+      _.each(adjacencyList, function(edges, parentElementLabel) {
+          elements.push(makeElement(parentElementLabel));
+  
+          _.each(edges as any, function(childElementLabel) {
+              links.push(makeLink(parentElementLabel, childElementLabel));
+          });
+      });
+  
+      // Links must be added after all the elements. This is because when the links
+      // are added to the graph, link source/target
+      // elements must be in the graph already.
+      return elements.concat(links);
+  }
+  
+function makeLink(parentElementLabel, childElementLabel) {
+  
+      return new joint.dia.Link({
+          source: { id: parentElementLabel },
+          target: { id: childElementLabel },
+          attrs: { '.marker-target': { d: 'M 4 0 L 0 2 L 4 4 z' } },
+          smooth: true
+      });
+  }
+  
+  function makeElement(label) {
+  
+      var maxLineLength = _.max(label.split('\n'), function(l:any) { return l.length; }).length;
+  
+      // Compute width/height of the rectangle based on the number 
+      // of lines in the label and the letter size. 0.6 * letterSize is
+      // an approximation of the monospace font letter width.
+      var letterSize = 8;
+      var width = 10;
+      var height = 10;
+  
+      return new joint.shapes.basic.Rect({
+          id: label,
+          size: { width: width, height: height },
+          attrs: {
+              text: { text: label, 'font-size': letterSize, 'font-family': 'monospace' },
+              rect: {
+                  width: width, height: height,
+                  rx: 5, ry: 5,
+                  stroke: '#555'
+              }
+          },
+          ports:{
+            groups:{
+              'a':{
+                position: {
+                    name: 'ellipseSpread',
+                    args: {
+                        dx: 1,
+                        dy: 1,
+                        dr: 1,
+                        startAngle: 10,
+                        step: 10,
+                        compensateRotation: false
+                    }
+                }
+              }
+            }
+          }
+      } as any);
+  }
 
-var tree = d3.tree()
-    .size([2 * Math.PI, 500])
-    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+  var name:string = "A"
+    var adjacencyList = {
+    }
 
-d3.csv("./assets/flare.csv", function(error, data) {
-  if (error) throw error;
+    var recu = (depth: number, name: string) => {
+      if(depth > 9) return;
+      adjacencyList[name] = [];
+      var parentName:string = name.substr(0,name.length - 1);
+      if (parentName)
+        adjacencyList[parentName].push(name);
+      recu(depth + 1, name + "0")
+      recu(depth + 1, name + "1")
+    }
+    recu(0, name)
 
-  var root = tree(stratify(data));
+  var cells = buildGraphFromAdjacencyList(adjacencyList);
+  graph.resetCells(cells);
 
-  var link = g.selectAll(".link")
-    .data(root.links())
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", d3.linkRadial()
-          .angle(function(d) { return d.x; })
-          .radius(function(d) { return d.y; }));
-
-  var node = g.selectAll(".node")
-    .data(root.descendants())
-    .enter().append("g")
-      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-      .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
-
-  node.append("circle")
-      .attr("r", 2.5);
-
-  node.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", function(d) { return d.x < Math.PI === !d.children ? 6 : -6; })
-      .attr("text-anchor", function(d) { return d.x < Math.PI === !d.children ? "start" : "end"; })
-      .attr("transform", function(d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
-      .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
-});
-
-function radialPoint(x, y) {
-  return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
-}
-
+    var graphBBox = joint.layout.DirectedGraph.layout(graph, {
+      nodeSep: 15,
+      edgeSep: 10,
+      rankSep: 10,
+      rankDir: "LR"
+  });
+  console.log('x:', graphBBox.x, 'y:', graphBBox.y)
+  console.log('width:', graphBBox.width, 'height:', graphBBox.height);
   }
 }
