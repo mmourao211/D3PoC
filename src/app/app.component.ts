719b9aa60f05b3fa916c7f9659637e6a269b742c
mmourao211 <sodwanted@gmail.com>
2017-10-26 19:33:39 +0300

    visjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import * as sigma from 'sigma'
+import * as vis from 'vis'
+import { Network } from 'vis';
 
 @Component({
   selector: 'app-root',
@@ -16,47 +17,22 @@ export class AppComponent {
     function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-        // Let's first initialize sigma:
-        var s = new sigma('cy');
-        
-            // Finally, let's ask our sigma instance to refresh:
+          var data:vis.Data = {nodes: [], edges: []};
             
             var initialName:string = 'A'
             
-            var recu = (depth: number, name: string, parentX: number) => {
-              if(depth > 15) return;
-              var newX = name.length == 1 ? 0 : (name.endsWith('0')? (parentX - 100.0/Math.pow(2,depth)) :(parentX + 100.0/Math.pow(2,depth)) );
-              s.graph.addNode({
-                // Main attributes:
-                id: name,
-                label: name,
-                // Display attributes:
-                size: 1000.0/depth,
-                color: '#f00',
-                x: newX ,
-                y: depth
-              })
+            var recu = (depth: number, name: string) => {
+              if(depth > 8) return;
+              (data.nodes as any[]).push({id: name, label: name})
               var parentName = name.substr(0,name.length - 1);
               if(parentName)
-                s.graph.addEdge({
-                  id: parentName + name,
-                  // Reference extremities:
-                  source: parentName,
-                  target: name
-                });
-              recu(depth + 1, name + "0", newX);
-              recu(depth + 1, name + "1", newX);
+                (data.edges as any[]).push({id: parentName + name, from: parentName, to: name})
+              recu(depth + 1, name + "0");
+              recu(depth + 1, name + "1");
               
             }
-            recu(1, initialName, 0);
-            s.settings({
-              // drawEdges: false,
-              // batchEdgesDrawing: true,
-              hideEdgesOnMove: true,
-              drawLabels: false,
-              minNodeSize: 0.001
-            })
-            s.camera.ratio = 0.01;
-            s.refresh();
-  }
+            recu(1, initialName);
+            var n = new Network(document.getElementById("cy"), data, {physics:false, layout:{improvedLayout: true}});
+            n.focus("A", {scale: 10})
+          }
 }
